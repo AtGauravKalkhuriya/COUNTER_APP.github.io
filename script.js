da0756501e43f8050ad4243a7d732b12212e27bc
Gaurav Kalkhuriya <128665446+AtGauravKalkhuriya@users.noreply.github.com>
2023-07-18 09:38:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const countValue = document.querySelector('#counter');
+
+
+let decrement = function(){
+  let value =   parseInt(countValue.innerText);
+  value = value-1;
+  countValue.innerText = value;
+
+}
+
+
+
+
+let increment = function(){
+    let value =   parseInt(countValue.innerText);
+    value = value+1;
+    countValue.innerText = value;
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
